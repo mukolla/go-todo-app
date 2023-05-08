936e8db746cb2a92992fc5462076db14fef8452b
mukolla <mukolla@gmail.com>
2023-05-08 23:59:35 +0300

    code

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,13 @@
-package cmd
+package main
+
+import (
+	"github.com/mukolla/go-todo-app"
+	"log"
+)
+
+func main() {
+	srv := new(todo.Server)
+	if err := srv.Run("8000"); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
