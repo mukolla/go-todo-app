b58b7cecf51ea02b0d1dc52c620c2cd515f195f2
mukolla <mukolla@gmail.com>
2023-05-09 14:11:56 +0300

    add: repos, services, handlers

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"github.com/mukolla/go-todo-app"
 	"github.com/mukolla/go-todo-app/pkg/handler"
+	"github.com/mukolla/go-todo-app/pkg/repository"
+	"github.com/mukolla/go-todo-app/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewService()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
