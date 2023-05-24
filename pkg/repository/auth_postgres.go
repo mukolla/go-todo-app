d256dc252a6f156d9cd735b9f068f2c527514f8e
mukolla <mukolla@gmail.com>
2023-05-24 15:14:18 +0300

    add middleware JWT auth token

--- pkg/repository/auth_postgres.go
+++ pkg/repository/auth_postgres.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	"github.com/mukolla/go-todo-app"
-	"github.com/sirupsen/logrus"
 )
 
 type AuthPostgres struct {
@@ -19,7 +18,7 @@ func (r *AuthPostgres) CreateUser(user todo.User) (int, error) {
 	var id int
 	query := fmt.Sprintf("INSERT INTO %s (name, username, password_hash) values ($1, $2, $3) RETURNING id", userTable)
 
-	logrus.Println(r.db.DB)
+	//logrus.Println(r.db.DB)
 
 	row := r.db.QueryRow(query, user.Name, user.Username, user.Password)
 	if err := row.Scan(&id); err != nil {
